0fff1ec97df8aa1e4ecdd41f227906f4f66a40ef
Chayarob <chayarob.c@silverlakeaxis.com>
2025-08-19 15:22:10 +0700

    Add stats.html

---
+++ object.js
@@ -0,0 +1,51 @@
+
+const classColor = [
+    { 
+        type: 'Swordcraft',
+        color: '#dde100ff',
+        selectedColor: '#8a8f00' // deeper golden green
+    },
+    {
+        type: 'Runecraft',
+        color: '#5e78feff',
+        selectedColor: '#2b3cbf' // deeper blue
+    },
+    {
+        type: 'Abysscraft',
+        color: '#fb4c84ff',
+        selectedColor: '#b3124b' // richer magenta-red
+    },
+    {
+        type: 'Portalcraft',
+        color: '#74bed1ff',
+        selectedColor: '#2d7f96' // deep teal-blue
+    },
+    {
+        type: 'Havencraft',
+        color: '#d0be6dff',
+        selectedColor: '#8a7835' // muted antique gold
+    },
+    {
+        type: 'Dragoncraft',
+        color: '#ff8f1d',
+        selectedColor: '#b3540f' // warm dragon orange
+    },
+    {
+        type: 'Forestcraft',
+        color: '#93c6a1ff',
+        selectedColor: '#4f7d59' // earthy forest green
+    }
+]
+
+const expansionList = [
+    'Legends Rise',
+    'Infinity Evolved'
+];
+
+const groupList = [
+    'Emerald',
+    'Topaz',
+    'Ruby',
+    'Sapphire',
+    'Diamond'
+]
